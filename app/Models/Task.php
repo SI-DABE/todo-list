c44d28b7d8b294a586084fc4e994692ad11f22ee
Diego Marczal <dmarczal@gmail.com>
2023-09-25 21:57:26 -0300

    Setup PHPCS

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,20 +5,24 @@ namespace App\Models;
 use App\Models\Base;
 use App\Lib\Validations;
 
-class Task extends Base {
+class Task extends Base
+{
     private const DB_PATH = '../database/tasks.txt';
-    
+
     private string $name;
 
-    public function __construct(string $name = '') {
+    public function __construct(string $name = '')
+    {
         $this->name = trim($name);
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function validates() {
+    public function validates()
+    {
         Validations::notEmpty($this->name, 'name', $this->errors);
 
         // if (strlen($this->name) < 3) {
@@ -29,22 +33,26 @@ class Task extends Base {
         // }
     }
 
-    public function save() {
+    public function save()
+    {
         if ($this->isValid()) {
-            file_put_contents(self::DB_PATH,
-                              $this->name . PHP_EOL,
-                              FILE_APPEND | LOCK_EX);
+            file_put_contents(
+                self::DB_PATH,
+                $this->name . PHP_EOL,
+                FILE_APPEND | LOCK_EX
+            );
             return true;
         }
 
         return false;
     }
 
-    public static function all() {
+    public static function all()
+    {
         $tasks = [];
-        
+
         $tasksFromFile = file(self::DB_PATH, FILE_IGNORE_NEW_LINES);
-        foreach($tasksFromFile as $taskName) {
+        foreach ($tasksFromFile as $taskName) {
             $tasks[] = new Task($taskName);
         }
 
