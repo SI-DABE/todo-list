d6f86a2b1f1a54f274187c4cef992e5adebb18bd
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-10-03 19:32:24 -0300

    type; add controuler and view files: 	app/Controllers/NotFoundController.php 	app/views/NotFound.phtml

---
+++ app/Controllers/NotFoundController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Controllers;
+use App\Controllers\BaseController;
+class NotFoundController extends BaseController{
+    public function index(){
+        $this->render('NotFound');
+    }
+}
