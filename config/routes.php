ed754cd783371272163013da1e73158748d36900
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-10-03 19:39:16 -0300

    type: config file: config/routes.php description: add a notfound listener.

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,9 @@
 <?php
 
 use Core\Routes\Route;
-
+use App\Controllers\NotFoundController;
 Route::get('/tasks',     [App\Controllers\TasksController::class, 'index']);
 Route::get('/tasks/:id', [App\Controllers\TasksController::class, 'show']);
 Route::post('/tasks',    [App\Controllers\TasksController::class, 'create']);
 Route::delete('/tasks',  [App\Controllers\TasksController::class, 'destroy']);
+Route::putErrorHandler('not_found',[App\Controllers\NotFoundController::class,'index']);
