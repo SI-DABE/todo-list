a6fc3bd10431ec18b9ef124804f4024db1deed9b
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-10-10 17:02:26 -0300

    type: modify config file file: config/routes.php description: move error controller definitions to before router definitions. reason: new subroutes heir error controllers from her parents.

--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,8 @@
 use Core\Routes\Route;
 use App\Controllers\NotFoundController;
 $rootRoute = new Route;
+$rootRoute->putErrorController('not_found', [App\Controllers\NotFoundController::class,'index']);
 $rootRoute->get('/tasks',     [App\Controllers\TasksController::class, 'index']);
 $rootRoute->get('/tasks/:id', [App\Controllers\TasksController::class, 'show']);
 $rootRoute->post('/tasks',    [App\Controllers\TasksController::class, 'create']);
 $rootRoute->delete('/tasks',  [App\Controllers\TasksController::class, 'destroy']);
-$rootRoute->putErrorController('not_found', [App\Controllers\NotFoundController::class,'index']);
